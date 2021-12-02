3d9b36836a56c502b642b6194d0331316b5c6165
Eric Echemane <eechemane29@gmail.com>
2021-12-02 22:28:26 +0800

    Added link tag with rel='author' at document level

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,7 @@ class MyDocument extends Document {
                 <Head>
                     <title> Text Portrait Generator by Eric Echemane </title>
                     <meta name='keywords' content='css, portrait, css portrait, css image portrait, css text portrait image download, eric echemane' />
+                    <link rel="author" href="https://github.com/EricEchemane/CSS-Text-Portrait-Generator" />
                     <script src='https://cdn.jsdelivr.net/npm/html2canvas@1.3.3/dist/html2canvas.min.js'></script>
                 </Head>
                 <body>
